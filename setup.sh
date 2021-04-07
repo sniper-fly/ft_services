d6796fd32a943add59e4c6980fe7ebab6803d5e2
rnakai <rnakai@student.42tokyo.jp>
2021-04-07 16:18:20 +0900

    create setup.sh to install metallb

---
+++ setup.sh
@@ -0,0 +1,8 @@
+minikube start --vm-drive=docker --extra-config=apiserver.service-node-port-range=1-65535
+
+#install MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
