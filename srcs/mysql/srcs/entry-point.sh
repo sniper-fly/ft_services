d26658b366dadb22706c6d99ff0453f9822c1632
rnakai <rnakai@student.42tokyo.jp>
2021-04-10 15:43:48 +0900

    mysqldの3306ポートをopenになるように--skip-networkオプションを調整

--- srcs/mysql/srcs/entry-point.sh
+++ srcs/mysql/srcs/entry-point.sh
@@ -11,6 +11,14 @@ mysql -e "FLUSH PRIVILEGES;"
 rc-service mariadb stop
 
 #start mysqld
-/usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --pid-file=/run/mysqld/mariadb.pid
+/usr/bin/mysqld \
+	--user=mysql \
+	--datadir=/var/lib/mysql \
+	--plugin-dir=/usr/lib/mariadb/plugin \
+	--pid-file=/run/mysqld/mariadb.pid \
+	--port=3306 \
+	--log-error=/var/log/mysqld.log \
+	--log-warnings=4 \
+	--skip-networking=0
 
 sleep infinity
