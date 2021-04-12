8579d04a43737e28c3349e4e71b8306b50cf3800
rnakai <rnakai@student.42tokyo.jp>
2021-04-12 23:31:31 +0900

    httpsなどに対応

--- srcs/wordpress/srcs/wp_init.sh
+++ srcs/wordpress/srcs/wp_init.sh
@@ -3,30 +3,35 @@
 rc-status
 rc-service php-fpm7 start
 
-cd /var/www/localhost/htdocs/wordpress
-# wp core install --url=http://wordpress-service:5050/wordpress/ \
-# 	--title=test \
-# 	--admin_user=admin \
-# 	--admin_password=admin \
-# 	--admin_email=admin@example.com  \
-# 	--allow-root \
-# 	--path=/usr/share/webapps/wordpress/
+cd /www/wordpress
+
+sleep 10
+
+wp db create
+
+wp core install \
+	--url=https://192.168.49.2:5050 \
+	--title=test \
+	--admin_user=admin \
+	--admin_password=admin \
+	--admin_email=admin@example.com
+	# --allow-root \
 	# && wp language core install ja --allow-root --path=/usr/share/webapps/wordpress/ \
 	# && wp site switch-language ja --allow-root --path=/usr/share/webapps/wordpress/ \
 wp user create eren eren@example.com \
 	--role=editor \
-	--user_pass=eren \
-	--allow-root \
+	--user_pass=eren
+	# --allow-root \
 	# --path=/usr/share/webapps/wordpress/
 wp user create armin armin@example.com \
 	--role=author \
-	--user_pass=armin \
-	--allow-root \
+	--user_pass=armin
+	# --allow-root \
 	# --path=/usr/share/webapps/wordpress/
-wp user create mikasa armin@example.com \
+wp user create mikasa mikasa@example.com \
 	--role=author \
-	--user_pass=mikasa \
-	--allow-root \
+	--user_pass=mikasa
+	# --allow-root \
 	# --path=/usr/share/webapps/wordpress/
 
 nginx
