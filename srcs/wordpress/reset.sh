8b750f37ce1614b256a59bf4789e04e43b71fc9f
rnakai <rnakai@student.42tokyo.jp>
2021-04-12 23:22:52 +0900

    databaseも消せるようにreset.shを改良

--- srcs/wordpress/reset.sh
+++ srcs/wordpress/reset.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-kubectl delete pod wordpress-pod
+kubectl delete pod wordpress-pod mysql-pod
 eval $(minikube docker-env)
 docker rmi rnakai/wordpress:v1
 docker build -t rnakai/wordpress:v1 .
 kubectl apply -f wordpress-pod.yml
+kubectl apply -f ../mysql/mysql-pod.yml
