a5e1af70284f45b43b5da958b5fa6d743c625199
rnakai <rnakai@student.42tokyo.jp>
2021-04-14 11:13:00 +0900

    deploymentの機能を使って、mysqlのpodができていないときは再起動するようにした

--- srcs/wordpress/srcs/wp_init.sh
+++ srcs/wordpress/srcs/wp_init.sh
@@ -5,15 +5,22 @@ rc-service php-fpm7 start
 
 cd /www/wordpress
 
-## mysql-serviceの準備ができるまで待つ
-sleep 10
-
 wp core install \
 	--url=https://192.168.49.2:5050 \
 	--title=test \
 	--admin_user=admin \
 	--admin_password=admin \
 	--admin_email=admin@example.com
+
+# if mysql service is not ready, exit
+connection_result=$?
+echo "-----------connection result--------------"
+echo $connection_result
+echo "------------------------------------------"
+if [ $connection_result -ne 0 ] ; then
+	exit $connection_result
+fi
+
 wp user create eren eren@example.com \
 	--role=editor \
 	--user_pass=eren
