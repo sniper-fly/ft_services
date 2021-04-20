67ebbb5a039408cf1ec21d483485b4cfe3fb07ef
rnakai <rnakai@student.42tokyo.jp>
2021-04-20 23:18:18 +0900

    grafanaディレクトリ末尾のバージョン名を削除

--- srcs/grafana/srcs/grafana_init.sh
+++ srcs/grafana/srcs/grafana_init.sh
@@ -3,6 +3,6 @@
 #launch telegraf
 /telegraf-1.17.2/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
 
-/grafana-${GRAFANA_VERSION}/bin/grafana-server --homepath /grafana-${GRAFANA_VERSION} &
+/grafana/bin/grafana-server --homepath /grafana &
 
 sleep infinity
