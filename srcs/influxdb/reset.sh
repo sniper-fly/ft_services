70ad10ec022b71089237bac86120a6266cc24351
rnakai <rnakai@student.42tokyo.jp>
2021-04-28 18:41:44 +0900

    influxdbのresetの改善

--- srcs/influxdb/reset.sh
+++ srcs/influxdb/reset.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 eval $(minikube docker-env)
+kubectl delete -f influxdb-pod.yml
+while [ 1 ]
+do
+	docker rmi rnakai/influxdb:v1 && break
+	sleep 5
+done
 docker build -t rnakai/influxdb:v1 .
+kubectl apply -f influxdb-pod.yml
